0a372417d5db6aae3160dcc96962a031e1042fa2
Bharani <bharani5895@gmail.com>
2026-07-29 21:22:14 +0530

    Time: 0 ms (100%), Space: 19.4 MB (18.09%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -5,8 +5,8 @@ class Solution:
             if digits[i] < 9:
                 digits[i] += 1
                 return digits
-            
-            digits[i] = 0  
+            else:
+                digits[i] = 0  
 
         
         return [1] + digits
