9d2863b5eb92e94dec20b1f8f718af87ced2555c
Bharani <bharani5895@gmail.com>
2026-08-07 12:12:13 +0530

    Time: 399 ms (23.56%), Space: 19.4 MB (15.24%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.py
+++ 0016-3sum-closest/0016-3sum-closest.py
@@ -3,8 +3,7 @@ class Solution:
         nums.sort()
         dis=float('inf')
         for i in range(len(nums)-2):
-            if i>0 and nums[i]==nums[i-1]:
-                continue
+            
             l,r=i+1,len(nums)-1
             
             while l<r:
