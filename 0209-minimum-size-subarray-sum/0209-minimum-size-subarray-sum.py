7d0320e21aa49b8ebb30817e64629bed34ffbf27
Bharani <bharani5895@gmail.com>
2026-08-07 22:03:33 +0530

    Time: 15 ms (75.91%), Space: 30.5 MB (41.39%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -1,18 +1,12 @@
 class Solution:
-    def minSubArrayLen(self, target: int, nums: list[int]) -> int:
-        left = 0
-        current_sum = 0
-        min_len = float('inf')
-
-        # Expand the window using the right pointer
-        for right in range(len(nums)):
-            current_sum += nums[right]
-
-            # Shrink the window from the left while the sum condition is satisfied
-            while current_sum >= target:
-                min_len = min(min_len, right - left + 1)
-                current_sum -= nums[left]
-                left += 1
-
-        # Return min_len if a valid window was found, otherwise 0
-        return min_len if min_len != float('inf') else 0
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        total=0
+        l=0
+        res=float("inf")
+        for r in range(len(nums)):
+            total+=nums[r]
+            while total >=target:
+                res=min(r-l+1,res)
+                total-=nums[l]
+                l+=1
+        return 0 if res==float("inf") else res
