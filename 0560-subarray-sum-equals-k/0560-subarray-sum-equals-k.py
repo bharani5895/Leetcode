b78be66d7ded82fd8995eac39b071b1e4b5adeac
Bharani <bharani5895@gmail.com>
2026-08-08 16:06:11 +0530

    Time: 27 ms (91.07%), Space: 21.9 MB (56.69%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,24 +1,22 @@
 class Solution:
     def subarraySum(self, arr: List[int], k: int) -> int:
-        psa=0
-        hash_map={}
-        count=0
-        hash_map[0]=1
-        for i in range(len(arr)):
-            psa+=arr[i]
 
-            
-            a=psa
-            if a-k in hash_map:
-                count+=hash_map.get(a-k)
+        
+        
 
+        map = {0: 1}
+        
+        cursum = 0
+        count = 0
+        
+        for x in arr:
+            cursum += x
             
-            if a in hash_map:
-                hash_map[a]=hash_map.get(a,0)+1
-                
-            else:
-                hash_map[a]=1
+            need = cursum - k
             
+            if need in map:
+                count = count + map[need]
+            
+            map[cursum] = map.get(cursum, 0) + 1
+        
         return count
-        
-        
