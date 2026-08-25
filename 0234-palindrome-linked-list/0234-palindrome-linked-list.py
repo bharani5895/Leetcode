9dbdd56fac242596000a8f2315ad32cb5de8ff15
Bharani <bharani5895@gmail.com>
2026-08-25 10:04:14 +0530

    Time: 36 ms (50.56%), Space: 42.3 MB (86.11%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -6,40 +6,25 @@
 
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        if not head or not head.next:
-            return True
-
-        # Helper function to reverse the list starting from a given node
-        def reverselist(left: Optional[ListNode]) -> Optional[ListNode]:
-            prev = None
-            curr = left
-            while curr is not None:
-                after = curr.next  # Save next node before changing link
-                curr.next = prev   # Reverse pointer
-                prev = curr        # Advance prev
-                curr = after       # Advance curr
-            return prev
-
-        # 1. Find the middle of the linked list
-        fast = head
-        slow = head
-        prev = head
-
-        while fast is not None and fast.next is not None:
-            fast = fast.next.next
-            prev = slow
+        
+        slow = fast = head
+        while fast and fast.next:
             slow = slow.next
-
-        # 2. Reverse the second half (starting from slow)
-        second_half = reverselist(slow)
-
-        # 3. Compare the first half and reversed second half
-        p1 = head
-        p2 = second_half
-        while p2 is not None:
-            if p1.val != p2.val:
+            fast = fast.next.next    
+        
+        prev = None
+        curr = slow
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+            
+        left, right = head, prev
+        while right:
+            if left.val != right.val:
                 return False
-            p1 = p1.next
-            p2 = p2.next
-
+            left = left.next
+            right = right.next
+            
         return True
